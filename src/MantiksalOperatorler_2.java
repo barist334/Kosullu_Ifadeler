f55363717bf4aca092d84481882b481be5367f11
Baris Temiz <baristemiz334@gmail.com>
2021-12-11 16:52:00 +0300

    Initial commit

---
+++ src/MantiksalOperatorler_2.java
@@ -0,0 +1,22 @@
+public class MantiksalOperatorler_2 {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c;
+
+        //boolean kosul1 = (a < b);
+        //boolean kosul2 = (a > c);
+        //boolean sonuc = (kosul1 && kosul2);
+        //boolean sonuc = kosul1 || kosul2;
+        //System.out.println(!sonuc);
+
+        // ! Ters İşlem
+        //
+        //Sonucu tersine çevirir.
+
+        c = (a == b) ? 2 : 3;
+        System.out.println(c);
+        int not = 50;
+        System.out.println(not > 45 ? "Geçti" : "Kaldı");
+
+
+    }
+}
